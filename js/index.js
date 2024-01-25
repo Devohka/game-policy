caef6cf39e94684ba3d03082d50440cb4a72ed22
Devohka <110058157+Devohka@users.noreply.github.com>
2024-01-25 23:15:57 +0200

    tech-2

--- js/index.js
+++ js/index.js
@@ -6,6 +6,20 @@ const britain = document.querySelector("[data-britain]");
 
 const technologies = document.querySelector("[data-technologies]");
 const skip = document.querySelector("[data-skip]");
+const closeTech = document.querySelector("[data-close]");
+
+const sniper = document.querySelector("[data-sniper]");
+const plane = document.querySelector("[data-plane]");
+const ships = document.querySelector("[data-ships]");
+const rocket = document.querySelector("[data-rocket]");
+const sab = document.querySelector("[data-sab]");
+const tb = document.querySelector("[data-tb]");
+const debriefing = document.querySelector("[data-debriefing]");
+const tires = document.querySelector("[data-tires]");
+const radar = document.querySelector("[data-radar]");
+const anxienty = document.querySelector("[data-anxiety]");
+const airDefense = document.querySelector("[data-airDefense]");
+
 
 const menu = document.querySelector(".box-game");
 
@@ -23,11 +37,56 @@ function createHideMenu() {
     technologies.style.display = "block";
 };
 
+
+
+// Технології
+closeTech.addEventListener("click", () => {
+    tech.style.display = "none";
+});
+
 technologies.addEventListener("click", () => {
     tech.style.display = "block";
 });
 
 
+// самі технології
+
+const objTech = {
+    sniperO: false,
+    planeO: false,
+    shipsO: false,
+    rocketO: false,
+    sabO: false,
+    tbO: false,
+    debriefingO: false,
+    tiresO: false,
+    radarO: false,
+    anxientyO: false,
+    airDefenseO: false,
+
+    
+
+};
+
+sniper.addEventListener("click", () => {
+ const snip = objTech.sniperO = true;
+ localStorage.setItem("snip", JSON.stringify(snip));
+});
+
+airDefense.addEventListener("click", () => {
+   
+    if (localStorage.getItem("snip") === true) {
+        skip.textContent = "csdjvj";
+    } else {
+        skip.textContent = "sssdaadf";
+    }
+});
+
+
+
+
+
+// Країни
 
 britain.addEventListener("click", () => {
     britainM.style.borderColor = "green";
